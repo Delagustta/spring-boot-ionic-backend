a243699af0566185200fbb14066907870cb44a23
Gustavo Funchal <delagusttaf@gmail.com>
2019-03-05 22:03:44 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/gustavo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gustavo/cursomc/resources/CategoriaResource.java
@@ -21,7 +21,7 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET) //-------@GetMapping
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
@@ -39,7 +39,13 @@ public class CategoriaResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
-
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
  
 }
